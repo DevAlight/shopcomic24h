db59fe4df1ea414802c19458676ef295aa1ea6f4
Lam Quang Khai <khailq@devcamp.edu.vn>
2023-02-23 12:28:40 +0700

    abc

--- src/components/body/userInfo/OrderList.js
+++ src/components/body/userInfo/OrderList.js
@@ -35,13 +35,12 @@ const OrderList = () => {
         <Box sx={styleBox} >
             <h3>Danh sách order</h3>
             <Grid container >
-                <Grid item md={4} sm={4} xs={4}>
+                <Grid item md={3} sm={3} xs={3}>
                     <TableContainer >
                         <Table aria-label="simple table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>Mã order</TableCell>
-                                    <TableCell>Chi tiết</TableCell>
+                                    <TableCell>Mã order</TableCell>                                    
                                 </TableRow>
                             </TableHead>
                             <TableBody style={{ maxHeight: '300px', overflow: 'auto' }}>
@@ -52,12 +51,10 @@ const OrderList = () => {
                                         >
                                             <TableCell>
                                                 {value.orderCode !== undefined && value.orderCode !== null ? value.orderCode : "N/A"}
-                                            </TableCell>
-                                            <TableCell>
                                                 <Button onClick={value.orderCode !== undefined && value.orderCode !== null ? () => onDetailClick(value.order) : null}>
                                                     <i className="fa-solid fa-arrow-right"></i>
                                                 </Button>
-                                            </TableCell>
+                                            </TableCell>                                            
                                         </TableRow>
                                     ))
                                 ) : (
@@ -71,102 +68,103 @@ const OrderList = () => {
                         </Table>
                     </TableContainer>
                 </Grid>
-                <Grid item md={8} sm={8} xs={8}>
-                    {dataOrder ?
-                        <Grid container spacing={1}>
-                            <Grid item md={6} sm={12} xs={12}>
-                                <TextField name="name" label="Mã Order" variant="outlined" defaultValue={dataOrder.orderCode}
-                                    fullWidth disabled />
-                            </Grid>
-                            <Grid item md={6} sm={12} xs={12}>
-                                <TextField name="email" label="Email" variant="outlined" defaultValue={dataOrder.email}
-                                    fullWidth disabled/>
-                            </Grid>
-                            <Grid item md={6} sm={12}>
-                                <TextField name="orderDate" label="Ngày order" variant="outlined" defaultValue={(new Date(dataOrder.orderDate)).toLocaleDateString()}
-                                    fullWidth disabled />
-                            </Grid>
-                            <Grid item md={6} sm={12}>
-                                <TextField name="shippedDate" label="Ngày giao hàng" variant="outlined" type='date' defaultValue={new Date().toISOString().substring(0, 10)}
+                <Grid item md={9} sm={9} xs={9}>
+                    <Box sx={styleBox} >
+                        {dataOrder ?
+                            <Grid container spacing={1}>
+                                <Grid item md={6} sm={12} xs={12}>
+                                    <TextField name="name" label="Mã Order" variant="outlined" defaultValue={dataOrder.orderCode}
+                                        fullWidth disabled />
+                                </Grid>
+                                <Grid item md={6} sm={12} xs={12}>
+                                    <TextField name="email" label="Email" variant="outlined" defaultValue={dataOrder.email}
+                                        fullWidth disabled />
+                                </Grid>
+                                <Grid item md={6} sm={12} xs={12}>
+                                    <TextField name="orderDate" label="Ngày order" variant="outlined" defaultValue={(new Date(dataOrder.orderDate)).toLocaleDateString()}
+                                        fullWidth disabled />
+                                </Grid>
+                                <Grid item md={6} sm={12} xs={12}>
+                                    <TextField name="shippedDate" label="Ngày giao hàng" variant="outlined" type='date' defaultValue={new Date().toISOString().substring(0, 10)}
 
-                                    InputLabelProps={{ style: { shrink: true, } }}
-                                    fullWidth disabled/>
-                            </Grid>
-                            <Grid item md={6} sm={6}>
-                                <TextField name="status" label="Trạng thái" variant="outlined" defaultValue={dataOrder.status} select
+                                        InputLabelProps={{ style: { shrink: true, } }}
+                                        fullWidth disabled />
+                                </Grid>
+                                <Grid item md={6} sm={12} xs={12}>
+                                    <TextField name="status" label="Trạng thái" variant="outlined" defaultValue={dataOrder.status} select
 
-                                    fullWidth disabled>
-                                    <MenuItem value='open'>Open</MenuItem>
-                                    <MenuItem value='confirm'>Confirm</MenuItem>
-                                    <MenuItem value='cancel'>Cancel</MenuItem>
-                                </TextField>
-                            </Grid>
-                            <Grid item md={6} sm={6}>
-                                <TextField name="cost" label="Giá tiền" variant="outlined" defaultValue={dataOrder.cost.toLocaleString()}
-                                    fullWidth disabled />
-                            </Grid>
-                            <Grid item md={12} sm={12}>
-                                <TextField name="note" label="Mô tả" variant="filled" defaultValue={dataOrder.note} multiline
+                                        fullWidth disabled>
+                                        <MenuItem value='open'>Open</MenuItem>
+                                        <MenuItem value='confirm'>Confirm</MenuItem>
+                                        <MenuItem value='cancel'>Cancel</MenuItem>
+                                    </TextField>
+                                </Grid>
+                                <Grid item md={6} sm={6}>
+                                    <TextField name="cost" label="Giá tiền" variant="outlined" defaultValue={dataOrder.cost.toLocaleString()}
+                                        fullWidth disabled />
+                                </Grid>
+                                <Grid item md={12} sm={12}>
+                                    <TextField name="note" label="Mô tả" variant="filled" defaultValue={dataOrder.note} multiline
 
-                                    fullWidth rows={3} />
-                            </Grid>
-                            {dataOrder.orderDetails ?
-                                <>
-                                    {dataOrder.orderDetails.map((value, index) => {
-                                        return (
-                                            <Grid item md={12} sm={12} key={index}>
-                                                <Box sx={styleBox} >
-                                                    <Grid container sx={{ alignItems: 'center' }}>
-                                                        <Grid item md={2} sm={2}>
-                                                            <img src={value.imageUrl} style={{ maxHeight: '80px' }} alt='products' />
-                                                        </Grid>
-                                                        <Grid item md={3} sm={10}>
-                                                            <h4>{value.name}</h4>
-                                                        </Grid>
-                                                        <Grid item md={1} sm={3}>
-                                                            <h3> Qty:</h3>
-                                                        </Grid>
-                                                        <Grid item md={3} sm={3}>
-                                                            <ButtonGroup size="small">
+                                        fullWidth rows={3} />
+                                </Grid>
+                                {dataOrder.orderDetails ?
+                                    <>
+                                        {dataOrder.orderDetails.map((value, index) => {
+                                            return (
+                                                <Grid item md={12} sm={12} key={index}>
+                                                    <Box sx={styleBox} >
+                                                        <Grid container sx={{ alignItems: 'center' }}>
+                                                            <Grid item md={2} sm={2}>
+                                                                <img src={value.imageUrl} style={{ maxHeight: '80px' }} alt='products' />
+                                                            </Grid>
+                                                            <Grid item md={3} sm={10}>
+                                                                <h4>{value.name}</h4>
+                                                            </Grid>
+                                                            <Grid item md={2} sm={3}>
+                                                                <h4> Qty:</h4>
+                                                            </Grid>
+                                                            <Grid item md={2} sm={3}>
+                                                                <ButtonGroup size="small">
+                                                                    {/* <Button
+                                                                    onClick={() => onBtnMinusClick(value)}
+                                                                    >-</Button> */}
+                                                                    <Button disabled>{value.quantity}</Button>
+                                                                    {/* <Button
+                                                                    onClick={() => onBtnPlusClick(value)}
+                                                                    >+</Button> */}
+                                                                </ButtonGroup>
+                                                            </Grid>
+                                                            <Grid item md={3} sm={3}>
+                                                                <h5> {(value.promotionPrice * value.quantity).toLocaleString()} VnĐ</h5>
+                                                            </Grid>
+                                                            {/* <Grid item md={1} sm={3}>
                                                                 <Button
-                                                                // onClick={() => onBtnMinusClick(value)}
-                                                                >-</Button>
-                                                                <Button disabled>{value.quantity}</Button>
-                                                                <Button
-                                                                // onClick={() => onBtnPlusClick(value)}
-                                                                >+</Button>
-                                                            </ButtonGroup>
+                                                                    sx={{ padd: '20px' }}
+                                                                    variant="contained"
+                                                                    onClick={() => onBtnDelClick(value)}
+                                                                    color="error"
+                                                                >X</Button>
+                                                            </Grid> */}
                                                         </Grid>
-                                                        <Grid item md={2} sm={3}>
-                                                            <h5> {(value.promotionPrice * value.quantity).toLocaleString()} VnĐ</h5>
-                                                        </Grid>
-                                                        <Grid item md={1} sm={3}>
-                                                            <Button
-                                                                sx={{ padd: '20px' }}
-                                                                variant="contained"
-                                                                // onClick={() => onBtnDelClick(value)}
-                                                                color="error"
-                                                            >X</Button>
-                                                        </Grid>
-                                                    </Grid>
-                                                </Box>
-                                            </Grid>
+                                                    </Box>
+                                                </Grid>
 
-                                        )
-                                    })}
-                                </>
-                                :
+                                            )
+                                        })}
+                                    </>
+                                    :
+                                    null
+                                }
+                                <Grid item md={12} sm={12} style={{ textAlign: 'right' }}>
+                                    <h3>Tổng :  {total.toLocaleString()} VnĐ</h3>
+                                </Grid>
+                                <Grid item md={6} sm={6}>
+                                    <Button variant="contained" color="error" fullWidth onClick={() => { setDataOrder(null) }}>Quay về</Button>
+                                </Grid>
+                            </Grid> : null}
+                    </Box>
 
-                                null
-
-                            }
-                            <Grid item md={12} sm={12} style={{ textAlign: 'right' }}>
-                                <h3>Tổng :  {total.toLocaleString()} VnĐ</h3>
-                            </Grid>
-                            <Grid item md={6} sm={6}>
-                                <Button variant="contained" color="error" fullWidth onClick={() => { setDataOrder(null) }}>Quay về</Button>
-                            </Grid>
-                        </Grid> : null}
 
 
                 </Grid>
